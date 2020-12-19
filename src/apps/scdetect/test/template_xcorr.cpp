eb39ef5c43fcca653089ce5d0ea4c6c7ec651ebd
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-12-19 20:48:10 +0000

    TST: Update xcorr tests
    Remove tests testing the cross-correlation with different lags.

--- src/apps/scdetect/test/template_xcorr.cpp
+++ src/apps/scdetect/test/template_xcorr.cpp
@@ -26,8 +26,6 @@ BOOST_AUTO_TEST_CASE(xcorr, *utf::tolerance(test_unit_tolerance)) {
   };
 
   constexpr auto sampling_freq{1.0};
-  // TODO(damb): To be clarified with luca-s.
-  constexpr auto max_lag_samples{2};
 
   // template waveform trace
   constexpr auto size_tr1{3};
@@ -41,40 +39,65 @@ BOOST_AUTO_TEST_CASE(xcorr, *utf::tolerance(test_unit_tolerance)) {
   auto xcorr_result{utils::make_smart<Template::MatchResult>(
       sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{})};
 
-  BOOST_TEST_CHECK(
-      true == template_detail::XCorr(tr1.data(), size_tr1, tr2_01210.data(),
-                                     tr2_01210.size(), sampling_freq,
-                                     max_lag_samples, xcorr_result)
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_01210.data(),
+                               tr2_01210.size(), sampling_freq, xcorr_result)
 
   );
   BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
   BOOST_TEST_CHECK(xcorr_result->lag == 1.0);
 
+  // real-time trace
+  const std::array<double, 5> tr2_12100{1, 2, 1, 0, 0};
+
+  xcorr_result = utils::make_smart<Template::MatchResult>(
+      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
+
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_12100.data(),
+                               tr2_12100.size(), sampling_freq, xcorr_result)
+
+  );
+  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
+  BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
+
+  // real-time trace
+  const std::array<double, 5> tr2_00121{0, 0, 1, 2, 1};
+
+  xcorr_result = utils::make_smart<Template::MatchResult>(
+      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
+
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_00121.data(),
+                               tr2_00121.size(), sampling_freq, xcorr_result)
+
+  );
+  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
+  BOOST_TEST_CHECK(xcorr_result->lag == 2.0);
+
   tr1 = {1, 1, 1};
+  // real-time trace
   const std::array<double, 5> tr2_00000{0, 0, 0, 0, 0};
   sum_tr1 = SumTr1(tr1);
   squared_sum_tr1 = SquaredSumTr1(tr1);
   xcorr_result = utils::make_smart<Template::MatchResult>(
       sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
 
-  BOOST_TEST_CHECK(
-      false == template_detail::XCorr(tr1.data(), size_tr1, tr2_00000.data(),
-                                      tr2_00000.size(), sampling_freq,
-                                      max_lag_samples, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 0.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
+  BOOST_TEST_CHECK(false == template_detail::XCorr(
+                                tr1.data(), size_tr1, tr2_00000.data(),
+                                tr2_00000.size(), sampling_freq, xcorr_result));
 
   tr1 = {0, -1, 0};
+  // real-time trace
   const std::array<double, 5> tr2_00100{0, 0, 1, 0, 0};
   sum_tr1 = SumTr1(tr1);
   squared_sum_tr1 = SquaredSumTr1(tr1);
   xcorr_result = utils::make_smart<Template::MatchResult>(
       sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
 
-  BOOST_TEST_CHECK(
-      true == template_detail::XCorr(tr1.data(), size_tr1, tr2_00100.data(),
-                                     tr2_00100.size(), sampling_freq,
-                                     max_lag_samples, xcorr_result));
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_00100.data(),
+                               tr2_00100.size(), sampling_freq, xcorr_result));
   BOOST_TEST_CHECK(xcorr_result->coefficient == 0.5);
   BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
 
@@ -84,109 +107,25 @@ BOOST_AUTO_TEST_CASE(xcorr, *utf::tolerance(test_unit_tolerance)) {
   xcorr_result = utils::make_smart<Template::MatchResult>(
       sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
 
-  BOOST_TEST_CHECK(
-      true == template_detail::XCorr(tr1.data(), size_tr1, tr2_01210.data(),
-                                     tr2_01210.size(), sampling_freq,
-                                     max_lag_samples, xcorr_result));
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_01210.data(),
+                               tr2_01210.size(), sampling_freq, xcorr_result));
   BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
   BOOST_TEST_CHECK(xcorr_result->lag == 1.0);
 
   tr1 = {0, 0, 1};
+  // real-time trace
   const std::array<double, 5> tr2_10000{1, 0, 0, 0, 0};
   sum_tr1 = SumTr1(tr1);
   squared_sum_tr1 = SquaredSumTr1(tr1);
   xcorr_result = utils::make_smart<Template::MatchResult>(
       sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
 
-  BOOST_TEST_CHECK(
-      true == template_detail::XCorr(tr1.data(), size_tr1, tr2_10000.data(),
-                                     tr2_10000.size(), sampling_freq,
-                                     max_lag_samples, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == -2.0);
-
-  tr1 = {0, 0, 1};
-  sum_tr1 = SumTr1(tr1);
-  squared_sum_tr1 = SquaredSumTr1(tr1);
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  BOOST_TEST_CHECK(
-      true == template_detail::XCorr(tr1.data(), size_tr1, tr2_10000.data(),
-                                     tr2_10000.size(), sampling_freq,
-                                     max_lag_samples, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == -2.0);
-
-  // test max_lag_samples (single correlation)
-  tr1 = {0, 1, 0};
-  const std::array<double, 5> tr2_01000{0, 1, 0, 0, 0};
-  sum_tr1 = SumTr1(tr1);
-  squared_sum_tr1 = SquaredSumTr1(tr1);
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  BOOST_TEST_CHECK(true ==
-                   template_detail::XCorr(tr1.data(), size_tr1,
-                                          tr2_01000.data(), tr2_01000.size(),
-                                          sampling_freq, 0, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
-
-  // test max_lag_samples (lower limit)
-  tr1 = {1, 0, 1};
-  const std::array<double, 5> tr2_00202{0, 0, 2, 0, 2};
-  sum_tr1 = SumTr1(tr1);
-  squared_sum_tr1 = SquaredSumTr1(tr1);
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  BOOST_TEST_CHECK(true ==
-                   template_detail::XCorr(tr1.data(), size_tr1,
-                                          tr2_00202.data(), tr2_00202.size(),
-                                          sampling_freq, 1, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 0.5);
-  BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
-
-  tr1 = {1, 0, 1};
-  sum_tr1 = SumTr1(tr1);
-  squared_sum_tr1 = SquaredSumTr1(tr1);
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  // TODO(damb): To be clarified with luca-s.
-  BOOST_TEST_CHECK(true ==
-                   template_detail::XCorr(tr1.data(), size_tr1,
-                                          tr2_00202.data(), tr2_00202.size(),
-                                          sampling_freq, 2, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == 2.0);
-
-  // test max_lag_samples (upper limit)
-  tr1 = {0, 1, 0};
-  const std::array<double, 5> tr2_00001{0, 0, 0, 0, 1};
-  sum_tr1 = SumTr1(tr1);
-  squared_sum_tr1 = SquaredSumTr1(tr1);
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  BOOST_TEST_CHECK(true ==
-                   template_detail::XCorr(tr1.data(), size_tr1,
-                                          tr2_00001.data(), tr2_00001.size(),
-                                          sampling_freq, 2, xcorr_result));
+  BOOST_TEST_CHECK(true == template_detail::XCorr(
+                               tr1.data(), size_tr1, tr2_10000.data(),
+                               tr2_10000.size(), sampling_freq, xcorr_result));
   BOOST_TEST_CHECK(xcorr_result->coefficient == -0.5);
-  BOOST_TEST_CHECK(xcorr_result->lag == 2.0);
-
-  xcorr_result = utils::make_smart<Template::MatchResult>(
-      sum_tr1, squared_sum_tr1, size_tr1, Template::MatchResult::MetaData{});
-
-  // TODO(damb): To be clarified with luca-s.
-  BOOST_TEST_CHECK(true ==
-                   template_detail::XCorr(tr1.data(), size_tr1,
-                                          tr2_00001.data(), tr2_00001.size(),
-                                          sampling_freq, 3, xcorr_result));
-  BOOST_TEST_CHECK(xcorr_result->coefficient == 1.0);
-  BOOST_TEST_CHECK(xcorr_result->lag == 3.0);
+  BOOST_TEST_CHECK(xcorr_result->lag == 0.0);
 }
 
 } // namespace detect
