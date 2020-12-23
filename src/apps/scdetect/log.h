af4e9c7d038c29b6d2b36d145b3e927d3875becf
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-12-23 17:56:42 +0000

    ENH: Define macros for tagged log messages

--- src/apps/scdetect/log.h
+++ src/apps/scdetect/log.h
@@ -4,4 +4,17 @@
 #define SEISCOMP_COMPONENT DETECT
 #include <seiscomp/logging/log.h>
 
+#define SCDETECT_DEBUG_TAGGED(tag_str, format, ...)                            \
+  SEISCOMP_DEBUG("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_INFO_TAGGED(tag_str, format, ...)                             \
+  SEISCOMP_INFO("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_WARNING_TAGGED(tag_str, format, ...)                          \
+  SEISCOMP_WARNING("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_ERROR_TAGGED(tag_str, format, ...)                            \
+  SEISCOMP_WARNING("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_NOTICE_TAGGED(tag_str, format, ...)                           \
+  SEISCOMP_NOTICE("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_LOG_TAGGED(channel, tag_str, format, ...)                     \
+  SEISCOMP_LOG(channel, "[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+
 #endif // SCDETECT_APPS_SCDETECT_LOG_H_
