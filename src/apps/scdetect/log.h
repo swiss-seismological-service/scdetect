341cfe1807e0e22754725abf708718830c7d426a
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-12-28 19:20:11 +0000

    MAINT: Keep code DRY
    Use workaround centrally just once. Improve
    5a98a64b93d89ee2795cd3919c6ccf0503480bc5.
    
    Thanks @luca-s :).
    
    References: #12

--- src/apps/scdetect/log.h
+++ src/apps/scdetect/log.h
@@ -1,6 +1,7 @@
 #ifndef SCDETECT_APPS_SCDETECT_LOG_H_
 #define SCDETECT_APPS_SCDETECT_LOG_H_
 
+#include <seiscomp/core/strings.h>
 #define SEISCOMP_COMPONENT DETECT
 #include <seiscomp/logging/log.h>
 
@@ -11,17 +12,29 @@
 #define SCDETECT_LOG_NOTICE SEISCOMP_ERROR
 #define SCDETECT_LOG SEISCOMP_LOG
 
-#define SCDETECT_LOG_DEBUG_TAGGED(tag_str, format, ...)                        \
-  SCDETECT_LOG_DEBUG("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
-#define SCDETECT_LOG_INFO_TAGGED(tag_str, format, ...)                         \
-  SCDETECT_LOG_INFO("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
-#define SCDETECT_LOG_WARNING_TAGGED(tag_str, format, ...)                      \
-  SCDETECT_LOG_WARNING("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
-#define SCDETECT_LOG_ERROR_TAGGED(tag_str, format, ...)                        \
-  SCDETECT_LOG_ERROR("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+#define SCDETECT_LOG_DEBUG_TAGGED(tag_str, ...)                                \
+  SCDETECT_LOG_DEBUG((Core::stringify("[%s] ", tag_str.c_str()) +              \
+                      Core::stringify(__VA_ARGS__))                            \
+                         .c_str())
+#define SCDETECT_LOG_INFO_TAGGED(tag_str, ...)                                 \
+  SCDETECT_LOG_INFO((Core::stringify("[%s] ", tag_str.c_str()) +               \
+                     Core::stringify(__VA_ARGS__))                             \
+                        .c_str())
+#define SCDETECT_LOG_WARNING_TAGGED(tag_str, ...)                              \
+  SCDETECT_LOG_WARNING((Core::stringify("[%s] ", tag_str.c_str()) +            \
+                        Core::stringify(__VA_ARGS__))                          \
+                           .c_str())
+#define SCDETECT_LOG_ERROR_TAGGED(tag_str, ...)                                \
+  SCDETECT_LOG_ERROR((Core::stringify("[%s] ", tag_str.c_str()) +              \
+                      Core::stringify(__VA_ARGS__))                            \
+                         .c_str())
 #define SCDETECT_LOG_NOTICE_TAGGED(tag_str, format, ...)                       \
-  SCDETECT_LOG_NOTICE("[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
-#define SCDETECT_LOG_TAGGED(channel, tag_str, format, ...)                     \
-  SCDETECT_LOG(channel, "[%s] " format, tag_str.c_str(), ##__VA_ARGS__)
+  SCDETECT_LOG_NOTICE((Core::stringify("[%s] ", tag_str.c_str()) +             \
+                       Core::stringify(__VA_ARGS__))                           \
+                          .c_str())
+#define SCDETECT_LOG_TAGGED(channel, tag_str, ...)                             \
+  SCDETECT_LOG(channel, (Core::stringify("[%s] ", tag_str.c_str()) +           \
+                         Core::stringify(__VA_ARGS__))                         \
+                            .c_str())
 
 #endif // SCDETECT_APPS_SCDETECT_LOG_H_
