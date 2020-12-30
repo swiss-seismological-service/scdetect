15cbaa605549a172042fe057235aca9cc12befd5
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2020-12-30 19:55:36 +0000

    ENH: Provide facilities for integration testing

---
+++ src/apps/scdetect/test/integration.cpp
@@ -0,0 +1,104 @@
+#include "integration.h"
+
+#include <algorithm>
+
+#include <boost/filesystem.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace fs = boost::filesystem;
+
+namespace Seiscomp {
+namespace detect {
+namespace test {
+
+namespace cli {
+
+const std::string Flag::operator()() { return flag(); }
+
+ArgFlag::ArgFlag(const std::string &arg) : arg_{arg} {}
+const std::string ArgFlag::operator()() {
+  return std::string{flag() + "=" + arg_};
+}
+
+const std::string FlagDebug::flag() const { return "--debug"; }
+
+FlagOffline::FlagOffline() : ArgFlag{"1"} {}
+const std::string FlagOffline::flag() const { return std::string{"--offline"}; }
+
+FlagEp::FlagEp(const std::string &fpath) : ArgFlag{fpath} {}
+const std::string FlagEp::flag() const { return "--ep"; }
+
+FlagInventoryDB::FlagInventoryDB(const std::string &uri) : ArgFlag{uri} {}
+const std::string FlagInventoryDB::flag() const { return "--inventory-db"; }
+
+FlagEventDB::FlagEventDB(const std::string &uri) : ArgFlag{std::string{uri}} {}
+const std::string FlagEventDB::flag() const { return "--event-db"; }
+
+FlagRecordURL ::FlagRecordURL(const std::string &url) : ArgFlag{url} {}
+const std::string FlagRecordURL::flag() const { return "--record-url"; }
+
+FlagRecordStartTime::FlagRecordStartTime(const std::string &time_str)
+    : ArgFlag{time_str} {}
+const std::string FlagRecordStartTime::flag() const {
+  return "--record-starttime";
+}
+
+FlagRecordEndTime::FlagRecordEndTime(const std::string &time_str)
+    : ArgFlag{time_str} {}
+const std::string FlagRecordEndTime::flag() const { return "--record-endtime"; }
+
+FlagTemplateJSON::FlagTemplateJSON(const std::string &fpath) : ArgFlag{fpath} {}
+const std::string FlagTemplateJSON::flag() const { return "--template-json"; }
+
+} // namespace cli
+
+const std::string PathData(const std::string &fname) {
+  return fs::absolute(fs::path{"data"} / fname).string();
+}
+
+std::vector<char *>
+StringsToCStrings(const std::vector<std::string> &v_strings) {
+  std::vector<char *> v_cstrings{v_strings.size()};
+  std::transform(
+      v_strings.cbegin(), v_strings.cend(), v_cstrings.begin(),
+      [](const std::string &str) { return const_cast<char *>(str.c_str()); });
+  return v_cstrings;
+}
+
+/* --------------------------------------------------------------------------
+ */
+const std::string TempDirFixture::path_subdir{"scdetect"};
+
+TempDirFixture::TempDirFixture() : path_tempdir{create_path_unique()} {
+  int max_tries{5};
+  while (fs::exists(path_tempdir)) {
+    path_tempdir = create_path_unique();
+
+    if (!(--max_tries)) {
+      BOOST_FAIL("Failed to create temporary directory. Too many tries.");
+    }
+  }
+  if (!fs::create_directories(path_tempdir)) {
+    BOOST_FAIL(std::string{"Failed to create temporary directory: " +
+                           path_tempdir.string()});
+  }
+}
+
+TempDirFixture::~TempDirFixture() { fs::remove_all(path_tempdir); }
+
+const std::string TempDirFixture::path_tempdir_str() const {
+  return path_tempdir.string();
+}
+
+const char *TempDirFixture::path_tempdir_cstr() const {
+  return path_tempdir.c_str();
+}
+
+const fs::path TempDirFixture::create_path_unique() {
+  return fs::temp_directory_path() / TempDirFixture::path_subdir /
+         fs::unique_path();
+}
+
+} // namespace test
+} // namespace detect
+} // namespace Seiscomp
---
+++ src/apps/scdetect/test/integration.h
@@ -0,0 +1,111 @@
+#ifndef SCDETECT_APPS_SCDETECT_TEST_INTEGRATION_H_
+#define SCDETECT_APPS_SCDETECT_TEST_INTEGRATION_H_
+
+#include <string>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+
+namespace fs = boost::filesystem;
+
+namespace Seiscomp {
+namespace detect {
+namespace test {
+
+namespace cli {
+
+class Flag {
+public:
+  virtual const std::string flag() const = 0;
+  virtual const std::string operator()();
+};
+
+class ArgFlag : public Flag {
+public:
+  ArgFlag(const std::string &arg);
+  const std::string operator()() override;
+
+private:
+  std::string arg_;
+};
+
+class FlagDebug : public Flag {
+public:
+  const std::string flag() const override;
+};
+
+class FlagOffline : public ArgFlag {
+public:
+  FlagOffline();
+  const std::string flag() const override;
+};
+
+class FlagEp : public ArgFlag {
+public:
+  FlagEp(const std::string &fpath);
+  const std::string flag() const override;
+};
+
+class FlagInventoryDB : public ArgFlag {
+public:
+  FlagInventoryDB(const std::string &uri);
+  const std::string flag() const override;
+};
+
+class FlagEventDB : public ArgFlag {
+public:
+  FlagEventDB(const std::string &uri);
+  const std::string flag() const override;
+};
+
+class FlagRecordURL : public ArgFlag {
+public:
+  FlagRecordURL(const std::string &url);
+  const std::string flag() const override;
+};
+
+class FlagRecordStartTime : public ArgFlag {
+public:
+  FlagRecordStartTime(const std::string &time_str);
+  const std::string flag() const override;
+};
+
+class FlagRecordEndTime : public ArgFlag {
+public:
+  FlagRecordEndTime(const std::string &time_str);
+  const std::string flag() const override;
+};
+
+class FlagTemplateJSON : public ArgFlag {
+public:
+  FlagTemplateJSON(const std::string &fpath);
+  const std::string flag() const override;
+};
+
+} // namespace cli
+
+const std::string PathData(const std::string &fname = "");
+
+std::vector<char *>
+StringsToCStrings(const std::vector<std::string> &v_strings);
+
+/* -------------------------------------------------------------------------- */
+struct TempDirFixture {
+  TempDirFixture();
+  ~TempDirFixture();
+
+  const std::string path_tempdir_str() const;
+  const char *path_tempdir_cstr() const;
+
+  static const fs::path create_path_unique();
+  fs::path path_tempdir;
+
+private:
+  static const std::string path_subdir;
+};
+
+} // namespace test
+} // namespace detect
+} // namespace Seiscomp
+
+#endif // SCDETECT_APPS_SCDETECT_TEST_INTEGRATION_H_
