9bed18ffc7c367b4b788c992b17b6470c68a5633
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2021-01-13 13:40:44 +0000

    TST|ENH: Implement *simple* integration tests

---
+++ src/apps/scdetect/test/integration.cpp
@@ -0,0 +1,177 @@
+#include "seiscomp/datamodel/magnitude.h"
+#define SEISCOMP_TEST_MODULE test_integration_general
+#include <seiscomp/unittest/unittests.h>
+
+#include <cstdlib>
+#include <iterator>
+#include <ostream>
+#include <vector>
+
+#include <boost/algorithm/string/join.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/test/data/dataset.hpp>
+#include <boost/test/data/monomorphic.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/tools/fpc_tolerance.hpp>
+
+#include <seiscomp/datamodel/eventparameters.h>
+#include <seiscomp/io/archive/xmlarchive.h>
+
+#include "integration_utils.h"
+
+#include "../app.h"
+
+namespace utf = boost::unit_test;
+namespace utf_data = utf::data;
+namespace utf_tt = boost::test_tools;
+namespace fs = boost::filesystem;
+
+constexpr double test_unit_tolerance{0.000001};
+
+namespace Seiscomp {
+namespace detect {
+namespace test {
+
+namespace ds {
+
+struct Sample {
+  std::string path_template_config;
+  std::string path_inventory;
+  std::string path_catalog;
+  std::string path_records;
+
+  std::string path_expected;
+
+  fs::path path_sample;
+
+  std::vector<std::string> AsFlags(const fs::path &path_data) const {
+    std::vector<std::string> flags{
+        cli::to_string(cli::FlagTemplatesJSON{path_data / path_sample /
+                                              path_template_config}),
+        cli::to_string(
+            cli::FlagInventoryDB{path_data / path_sample / path_inventory}),
+
+        cli::to_string(cli::FlagRecordURL{
+            "file://" + (path_data / path_sample / path_records).string()}),
+
+        cli::to_string(
+            cli::FlagEventDB{path_data / path_sample / path_catalog})};
+
+    return flags;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Sample &sample);
+};
+
+std::ostream &operator<<(std::ostream &os, const Sample &sample) {
+  auto SamplePath = [&sample](const std::string &fname) {
+    return fname.empty() ? "" : (sample.path_sample / fname).string();
+  };
+
+  return os << "template_config: " << SamplePath(sample.path_template_config)
+
+            << ", inventory: " << SamplePath(sample.path_inventory)
+            << ", catalog: " << SamplePath(sample.path_catalog)
+            << ", records: " << SamplePath(sample.path_records)
+            << ", expected: " << SamplePath(sample.path_expected);
+}
+
+} // namespace ds
+
+// samples for parameterized testing
+using Samples = std::vector<ds::Sample>;
+Samples dataset{
+    {"templates.json", "inventory.scml", "catalog.scml", "data.mseed",
+     "expected.scml", /*path_sample=*/"integration-single-stream-simple"},
+    {"templates.json", "inventory.scml", "catalog.scml", "data.mseed",
+     "expected.scml",
+     /*path_sample=*/"integration-single-stream-simple-with-picks"}};
+
+BOOST_TEST_GLOBAL_FIXTURE(CLIPathData);
+
+BOOST_TEST_DECORATOR(*utf::tolerance(test_unit_tolerance))
+BOOST_DATA_TEST_CASE_F(TempDirFixture, integration_general,
+                       utf_data::make(dataset)) {
+
+  // prepare empty SQLite DB from template DB
+  const std::string db{"seiscomp_db.sqlite"};
+  fs::path path_db{path_tempdir / db};
+  try {
+    fs::copy_file(CLIPathData::path / db, path_db);
+  } catch (fs::filesystem_error &e) {
+    BOOST_FAIL("Failed to prepare database:" << e.what());
+  }
+
+  fs::path path_ep_result_scml{path_tempdir / "ep.scml"};
+
+  // prepare CLI flags
+  std::vector<std::string> flags_str{
+      "scdetect",
+      cli::to_string(cli::FlagDebug{}),
+      cli::to_string(cli::FlagOffline{}),
+      cli::to_string(cli::FlagTemplatesReload{}),
+      cli::to_string(cli::FlagPlugins{"dbsqlite3"}),
+      cli::to_string(cli::FlagDB{path_db}),
+      cli::to_string(cli::FlagEp{path_ep_result_scml}),
+      cli::to_string(cli::FlagAgencyId{"TEST"})};
+  auto flags_sample{sample.AsFlags(CLIPathData::path)};
+  flags_str.insert(std::end(flags_str), std::begin(flags_sample),
+                   std::end(flags_sample));
+
+  BOOST_TEST_MESSAGE("Running integration test with CLI args: "
+                     << boost::algorithm::join(flags_str, " "));
+  BOOST_TEST_MESSAGE("Path to temporary test data: " << path_tempdir);
+
+  auto StrToCStr = [](const std::string &str) {
+    char *ret{new char[str.size() + 1]};
+    std::strcpy(ret, str.c_str());
+    return ret;
+  };
+  std::vector<char *> flags_cstr;
+  flags_cstr.reserve(flags_str.size());
+  std::transform(flags_str.cbegin(), flags_str.cend(),
+                 back_inserter(flags_cstr), StrToCStr);
+
+  int retval{EXIT_FAILURE};
+  {
+    retval =
+        Application{static_cast<int>(flags_cstr.size()), flags_cstr.data()}();
+  }
+
+  for (size_t i = 0; i < flags_cstr.size(); ++i) {
+    delete[] flags_cstr[i];
+  }
+
+  BOOST_TEST_CHECK(EXIT_SUCCESS == retval);
+
+  auto ReadEventParameters = [](const fs::path &path,
+                                DataModel::EventParametersPtr &ep_ptr) {
+    BOOST_TEST_REQUIRE(!path.empty(), "Invalid path.");
+    IO::XMLArchive ar;
+    if (!ar.open(path.c_str())) {
+      BOOST_FAIL("Failed to open file: " << path);
+    }
+    ar >> ep_ptr;
+    ar.close();
+  };
+
+  // read detection results
+  DataModel::EventParametersPtr ep_result;
+  ReadEventParameters(path_ep_result_scml, ep_result);
+  BOOST_TEST_REQUIRE(ep_result, "Failed to read file: " << path_ep_result_scml);
+
+  // read expected result
+  DataModel::EventParametersPtr ep_expected;
+  fs::path path_ep_expected_scml{CLIPathData::path / sample.path_sample /
+                                 sample.path_expected};
+  ReadEventParameters(path_ep_expected_scml, ep_expected);
+  BOOST_TEST_REQUIRE(ep_expected,
+                     "Failed to read file: " << path_ep_expected_scml);
+
+  EventParametersCmp(ep_result, ep_expected);
+}
+
+} // namespace test
+} // namespace detect
+} // namespace Seiscomp
