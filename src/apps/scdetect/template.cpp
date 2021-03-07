c11262cbf8ea8131691e9366c70275318849da93
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2021-03-07 12:40:15 +0000

    ENH: Implement filter-based cross-correlation
    Prepare the `Template` waveform processor for the application of
    filter-based cross-correlation. Take cross-correlation filter delay into
    account.

--- src/apps/scdetect/template.cpp
+++ src/apps/scdetect/template.cpp
@@ -1,26 +1,12 @@
 #include "template.h"
 
 #include <algorithm>
-#include <cfenv>
-#include <cmath>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
 #include <memory>
-#include <sstream>
 #include <stdexcept>
 #include <string>
 
-#include <boost/algorithm/string/join.hpp>
-#include <boost/filesystem.hpp>
-
-#include <seiscomp/core/timewindow.h>
-#include <seiscomp/system/environment.h>
-#include <seiscomp/utils/files.h>
-
 #include "log.h"
-#include "processor.h"
+#include "seiscomp/core/timewindow.h"
 #include "settings.h"
 #include "utils.h"
 #include "waveform.h"
@@ -28,23 +14,18 @@
 namespace Seiscomp {
 namespace detect {
 
-Template::Template(const std::string &id, const Processor *p)
-    : WaveformProcessor{
-          p ? std::string{p->id() + settings::kProcessorIdSep + id} : id} {}
-
-TemplateBuilder Template::Create(const std::string &id, const Processor *p) {
-  return TemplateBuilder(id, p);
-}
+Template::Template(const GenericRecordCPtr &template_wf, const std::string &id,
+                   const Processor *p)
+    : WaveformProcessor{p ? std::string{p->id() + settings::kProcessorIdSep +
+                                        id}
+                          : id},
+      cross_correlation_{template_wf} {}
 
 void Template::set_filter(Filter *filter) {
   if (stream_state_.filter)
     delete stream_state_.filter;
 
   stream_state_.filter = filter;
-}
-
-const Core::TimeSpan Template::init_time() const {
-  return std::max(init_time_, waveform_end_ - waveform_start_);
 }
 
 bool Template::Feed(const Record *record) {
@@ -63,375 +44,68 @@ void Template::Reset() {
     delete tmp;
   }
 
+  cross_correlation_.Reset();
+
   WaveformProcessor::Reset();
 }
 
 void Template::Process(StreamState &stream_state, const Record *record,
                        const DoubleArray &filtered_data) {
-  const double *samples_template{
-      DoubleArray::ConstCast(waveform_->data())->typedData()};
-  const double *samples_trace{filtered_data.typedData()};
-  const int num_samples_template{waveform_->data()->size()};
-  const int num_samples_trace{filtered_data.size()};
 
-  MatchResultPtr result{utils::make_smart<MatchResult>(
-      waveform_sum_, waveform_squared_sum_, num_samples_template,
-      record->timeWindow())};
-
-  if (num_samples_template > num_samples_trace) {
-    set_status(Status::kWaitingForData,
-               num_samples_template - num_samples_trace);
-    return;
-  }
-
+  const auto n{static_cast<size_t>(filtered_data.size())};
   set_status(Status::kInProgress, 1);
 
-  if (debug_mode()) {
-    if (!Util::pathExists(debug_info_dir().string())) {
-      if (!Util::createPath(debug_info_dir().string())) {
-        SCDETECT_LOG_ERROR_PROCESSOR(this, "Failed to create directory: %s",
-                                     debug_info_dir().c_str());
-        set_status(Status::kError, 0);
-        return;
-      }
-    }
+  int start_idx{0};
+  Core::Time start{record->timeWindow().startTime()};
+  // check if processing start lies within the record
+  if (!stream_state_.initialized) {
+    start_idx =
+        std::max(0, static_cast<int>(n) -
+                        static_cast<int>(stream_state_.received_samples -
+                                         stream_state_.needed_samples));
+    double t = start_idx / n;
+    start =
+        record->startTime() + Core::TimeSpan{record->timeWindow().length() * t};
+  }
 
-    std::string fname_sep{"_"};
-    // dump template waveform
-    {
-      std::string fname{waveform_->streamID() + fname_sep +
-                        waveform_->startTime().iso() + fname_sep +
-                        waveform_->endTime().iso() + fname_sep +
-                        std::to_string(std::hash<std::string>{}(
-                            std::to_string(waveform_sampling_frequency_) +
-                            waveform_filter_)) +
-                        ".mseed"};
-      auto fpath{debug_info_dir() / fname};
-      if (!Util::fileExists(fpath.string())) {
-        std::ofstream ofs{fpath.string()};
-        waveform::Write(*waveform_.get(), ofs);
-      }
-
-      result->debug_info.path_template = fpath.string();
-    }
-
-    // dump real-time trace (filtered)
-    {
-      auto dump_me{utils::make_smart<GenericRecord>(*record)};
-      dump_me->setData(filtered_data.size(), filtered_data.typedData(),
-                       Array::DOUBLE);
-      dump_me->setSamplingFrequency(waveform_sampling_frequency_);
-      dump_me->dataUpdated();
-
-      std::string fname{record->streamID() + fname_sep +
-                        record->startTime().iso() + fname_sep +
-                        record->endTime().iso() + ".mseed"};
-      auto fpath{debug_info_dir() / fname};
-      std::ofstream ofs{fpath.string()};
-      waveform::Write(*dump_me.get(), ofs);
-
-      result->debug_info.path_trace = fpath.string();
+  double coefficient{std::nan("")}, lag_idx{0};
+  // determine the first maximum correlation coefficient
+  for (size_t i{static_cast<size_t>(start_idx)}; i < n; ++i) {
+    const double v{filtered_data[i]};
+    if (!isfinite(coefficient) || coefficient < v) {
+      coefficient = v;
+      lag_idx = i;
     }
   }
 
-  if (template_detail::XCorr(samples_template, num_samples_template,
-                             samples_trace, num_samples_trace,
-                             waveform_sampling_frequency_, result, this)) {
-    merge_processed(Core::TimeWindow{
-        record->startTime(),
-        Core::Time{
-            record->startTime() +
-            Core::TimeSpan{
-                (num_samples_trace - num_samples_template) /
-                waveform_sampling_frequency_ /* samples to seconds */}}});
-    set_status(WaveformProcessor::Status::kFinished, 100);
+  // take cross-correlation filter delay into account i.e. the template
+  // processor's result is referring to a time window shifted to the past
+  const auto t{
+      static_cast<double>(lag_idx - cross_correlation_.template_size()) /
+      (n + cross_correlation_.template_size())};
+  const Core::TimeSpan template_length{cross_correlation_.template_length()};
+  const Core::TimeWindow tw{start - template_length,
+                            record->endTime() - template_length};
 
-    EmitResult(record, result.get());
-    return;
-  }
-  set_status(WaveformProcessor::Status::kError, 0);
+  auto result{utils::make_smart<MatchResult>(coefficient, tw.length() * t, tw)};
+  EmitResult(record, result.get());
 }
 
 void Template::Fill(StreamState &stream_state, const Record *record,
                     DoubleArrayPtr &data) {
 
+  // TODO(damb): Allow target sampling frequency to be configurable if filter
+  // is in use.
   WaveformProcessor::Fill(stream_state, record, data);
 
   waveform::Demean(*data);
-
-  // resample (i.e. always downsample)
-  if (waveform_sampling_frequency_ != stream_state_.sampling_frequency) {
-    if (waveform_sampling_frequency_ < stream_state.sampling_frequency) {
-      // resample trace (real-time data)
-      auto tmp{
-          utils::make_unique<DoubleArray>(data->size(), data->typedData())};
-      waveform::Resample(*tmp, stream_state.sampling_frequency,
-                         waveform_sampling_frequency_, true);
-
-      data.reset(tmp.release());
-    } else {
-      // resample template waveform
-      auto resampled{utils::make_smart<GenericRecord>(*waveform_)};
-      waveform::Resample(*resampled, stream_state.sampling_frequency, true);
-      waveform_ = resampled;
-      waveform_sampling_frequency_ = stream_state.sampling_frequency;
-    }
-  }
+  cross_correlation_.Apply(data->size(), data->typedData());
 }
 
 void Template::InitStream(StreamState &stream_state, const Record *record) {
   WaveformProcessor::InitStream(stream_state, record);
-
-  stream_state.needed_samples =
-      std::max(static_cast<size_t>(waveform_->sampleCount()),
-               stream_state.needed_samples);
+  cross_correlation_.set_sampling_frequency(stream_state.sampling_frequency);
 }
-
-/* ------------------------------------------------------------------------- */
-Template::MatchResult::MatchResult(const double sum_template,
-                                   const double squared_sum_template,
-                                   const int num_samples_template,
-                                   const Core::TimeWindow &tw)
-    : num_samples_template{num_samples_template}, sum_template{sum_template},
-      squared_sum_template{squared_sum_template}, time_window{tw} {}
-
-std::string Template::MatchResult::DebugString() const {
-  std::ostringstream oss;
-  oss << "\"startTime\": \"" << time_window.startTime().iso()
-      << "\", \"endTime\": \"" << time_window.endTime().iso()
-      << "\", \"fit\": " << std::fixed << std::setprecision(6) << coefficient
-      << ", \"lag\": " << lag;
-
-  if (!debug_info.path_template.empty()) {
-    oss << ", \"pathTemplate\": \"" << debug_info.path_template << "\"";
-  }
-  if (!debug_info.path_trace.empty()) {
-    oss << ", \"pathTrace\": \"" << debug_info.path_trace << "\"";
-  }
-
-  return oss.str();
-}
-
-/* ------------------------------------------------------------------------- */
-TemplateBuilder::TemplateBuilder(const std::string &id, const Processor *p) {
-  // XXX(damb): Using `new` to access a non-public ctor; see also
-  // https://abseil.io/tips/134
-  product_ = std::unique_ptr<Template>(new Template{id, p});
-}
-
-TemplateBuilder &TemplateBuilder::set_waveform(
-    WaveformHandlerIfacePtr waveform_handler, const std::string &stream_id,
-    const Core::Time &wf_start, const Core::Time &wf_end,
-    const WaveformHandlerIface::ProcessingConfig &config,
-    Core::Time &wf_start_waveform, Core::Time &wf_end_waveform) {
-
-  product_->waveform_start_ = wf_start;
-  product_->waveform_end_ = wf_end;
-  product_->waveform_length_ = wf_end - wf_start;
-  product_->waveform_stream_id_ = stream_id;
-
-  // prepare waveform stream id
-  std::vector<std::string> wf_tokens;
-  Core::split(wf_tokens, stream_id, ".", false);
-  try {
-    product_->waveform_ =
-        waveform_handler->Get(wf_tokens[0], wf_tokens[1], wf_tokens[2],
-                              wf_tokens[3], wf_start, wf_end, config);
-
-    wf_start_waveform = product_->waveform_->startTime();
-    wf_end_waveform = product_->waveform_->endTime();
-  } catch (WaveformHandler::NoData &e) {
-    throw builder::NoWaveformData{
-        std::string{"Failed to load template waveform: "} + e.what()};
-  } catch (std::exception &e) {
-    throw builder::BaseException{
-        std::string{"Failed to load template waveform: "} + e.what()};
-  }
-  product_->waveform_sampling_frequency_ =
-      product_->waveform_->samplingFrequency();
-  product_->waveform_filter_ = config.filter_string;
-
-  const double *samples_template{
-      DoubleArray::ConstCast(product_->waveform_->data())->typedData()};
-
-  for (int i = 0; i < product_->waveform_->data()->size(); ++i) {
-    product_->waveform_sum_ += samples_template[i];
-    product_->waveform_squared_sum_ +=
-        samples_template[i] * samples_template[i];
-  }
-  return *this;
-}
-
-TemplateBuilder &TemplateBuilder::set_filter(WaveformProcessor::Filter *filter,
-                                             double init_time) {
-  product_->set_filter(filter);
-  product_->init_time_ = Core::TimeSpan{init_time};
-  return *this;
-}
-
-TemplateBuilder &TemplateBuilder::set_saturation_check(bool saturation_check,
-                                                       double thres) {
-  product_->set_saturation_check(saturation_check);
-  product_->set_saturation_threshold(thres);
-  return *this;
-}
-
-TemplateBuilder &
-TemplateBuilder::set_debug_info_dir(const boost::filesystem::path &path) {
-  product_->set_debug_info_dir(path);
-  return *this;
-}
-
-/* ------------------------------------------------------------------------- */
-namespace template_detail {
-
-bool XCorr(const double *tr1, const int size_tr1, const double *tr2,
-           const int size_tr2, const double sampling_freq,
-           Template::MatchResultPtr &result, const Processor *processor) {
-
-  /*
-   * Pearson correlation coefficient for time series X and Y of length n
-   *
-   *              sum((Xi-meanX) * (Yi-meanY))
-   * cc = --------------------------------------------------
-   *      sqrt(sum((Xi-meanX)^2)) * sqrt(sum((Yi-meanY)^2))
-   *
-   * Where sum(X)  is the sum of Xi for i=1 until i=n
-   *
-   * This can be rearranged in a form suitable for a single-pass algorithm
-   * (where the mean of X and Y are not needed)
-   *
-   *                 n * sum(Xi*Yi) - sum(Xi) * sum(Yi)
-   * cc = -----------------------------------------------------------
-   *      sqrt(n*sum(Xi^2)-sum(Xi)^2) * sqrt(n*sum(Yi^2)-sum(Yi)^2))
-   *
-   * For cross-correlation, where we have a short trace `tr1` which is
-   * correlated against a longer trace `tr2` at subsequent offset, we can
-   * pre-compute the parts that involve `tr1` and re-use them at each step of
-   * the cross-correlation:
-   *
-   *   sum_short = sum(Xi)
-   *   squared_sum_short = sum(Xi^2)
-   *   denominator_short = sqrt(n*squared_sum_short-(sum_short)^2)
-   *
-   * For the parts that involves the longer trace L alone we can compute them
-   * in a rolling fashion (removing first sample of previous iteration and
-   * adding the last sample of the new iteration):
-   *
-   *   sum_long = sum(Yi)
-   *   squared_sum_long = sum(Yi^2)
-   *   denominator_long = sqrt(n*squared_sum_long-(sum_long)^2)
-   *
-   * Finally, this is the equation at each step (offset) of cross-correlation:
-   *
-   *       n * sum(Xi*Yi) - sum_short  * sum_long
-   * cc = ---------------------------------------
-   *        denominator_short * denominator_long
-   *
-   * Unfortunately, we cannot optimize sum(Xi*Yi) and this will be a inner
-   * loop inside the main cross-correlation loop
-   */
-
-  auto SampleAtLong = [&size_tr2, &tr2](int idx_short, int lag) {
-    const int idx_long{idx_short + lag};
-    return (idx_long < 0 || idx_long >= size_tr2) ? 0 : tr2[idx_long];
-  };
-
-  if (size_tr2 < size_tr1) {
-    return false;
-  }
-
-  if (!result->squared_sum_template)
-    return false;
-
-  result->coefficient = std::nan("");
-  std::feclearexcept(FE_ALL_EXCEPT);
-
-  // do as much computation as possible outside the main cross-correlation loop
-  const auto &n = size_tr1;
-  const auto &sum_short = result->sum_template;
-  const auto &squared_sum_short = result->squared_sum_template;
-  const double denominator_short{
-      std::sqrt(n * squared_sum_short - sum_short * sum_short)};
-
-  if (!std::isfinite(denominator_short)) {
-    std::feclearexcept(FE_INVALID);
-    return false;
-  }
-
-  double sum_long{0};
-  double squared_sum_long{0};
-  for (int i = 0; i < n; ++i) {
-    double sample{SampleAtLong(i, -1)};
-    sum_long += sample;
-    squared_sum_long += sample * sample;
-  }
-
-  // cross-correlation loop
-  for (int lag = 0; lag <= size_tr2 - size_tr1; ++lag) {
-    // sum_long/squared_sum_long: remove the sample that has exited the current
-    // xcorr win and add the sample that has just entered the current xcorr win
-    const double last_sample_long{SampleAtLong(-1, lag)};
-    const double new_sample_long{SampleAtLong(n - 1, lag)};
-    sum_long += new_sample_long - last_sample_long;
-    squared_sum_long +=
-        new_sample_long * new_sample_long - last_sample_long * last_sample_long;
-
-    const double denominator_long{
-        std::sqrt(n * squared_sum_long - sum_long * sum_long)};
-
-    if (!std::isfinite(denominator_long)) {
-      continue;
-    }
-
-    double sum_short_long{0};
-    for (int i = 0; i < n; ++i) {
-      sum_short_long += tr1[i] * SampleAtLong(i, lag);
-    }
-
-    const double coeff{(n * sum_short_long - sum_short * sum_long) /
-                       (denominator_short * denominator_long)};
-
-    if (!std::isfinite(result->coefficient) ||
-        (std::isfinite(coeff) && coeff > result->coefficient)) {
-      result->sum_trace = sum_long;
-      result->squared_sum_trace = squared_sum_long;
-      result->sum_template_trace = sum_short_long;
-      result->coefficient = coeff;
-      result->lag = lag / sampling_freq; // samples to seconds
-    }
-  }
-
-  int fe{fetestexcept(FE_ALL_EXCEPT)};
-  if ((fe & ~FE_INEXACT) != 0) // we don't care about FE_INEXACT
-  {
-    std::vector<std::string> exceptions;
-    if (fe & FE_DIVBYZERO)
-      exceptions.push_back("FE_DIVBYZERO");
-    if (fe & FE_INVALID)
-      exceptions.push_back("FE_INVALID");
-    if (fe & FE_OVERFLOW)
-      exceptions.push_back("FE_OVERFLOW");
-    if (fe & FE_UNDERFLOW)
-      exceptions.push_back("FE_UNDERFLOW");
-
-    std::string msg{"Floating point exception during cross-correlation: "};
-    msg += boost::algorithm::join(exceptions, ", ");
-    if (processor) {
-      SCDETECT_LOG_WARNING_TAGGED(processor->id(), "%s", msg.c_str());
-    } else {
-      SCDETECT_LOG_WARNING(msg.c_str());
-    }
-  }
-
-  if (!std::isfinite(result->coefficient)) {
-    return false;
-  }
-
-  return true;
-}
-
-} // namespace template_detail
 
 } // namespace detect
 } // namespace Seiscomp
--- src/apps/scdetect/template.h
+++ src/apps/scdetect/template.h
@@ -11,56 +11,34 @@
 #include <seiscomp/datamodel/pick.h>
 #include <seiscomp/datamodel/stream.h>
 
-#include "builder.h"
+#include "filter/crosscorrelation.h"
 #include "waveform.h"
 #include "waveformprocessor.h"
 
 namespace Seiscomp {
 namespace detect {
 
-class TemplateBuilder;
-
 // Template waveform processor implementation
 // - implements filtering
-// - implements the actual cross-correlation algorithm
+// - applies the cross-correlation algorithm
 class Template : public WaveformProcessor {
 
-  Template(const std::string &id, const Processor *p = nullptr);
-
 public:
-  friend class TemplateBuilder;
-  static TemplateBuilder Create(const std::string &id,
-                                const Processor *p = nullptr);
+  // Creates a `Template` waveform processor from the template waveform
+  // `template_wf`
+  Template(const GenericRecordCPtr &template_wf, const std::string &id,
+           const Processor *p = nullptr);
 
   DEFINE_SMARTPOINTER(MatchResult);
   struct MatchResult : public Result {
-
-    MatchResult(const double sum_template, const double squared_sum_template,
-                const int num_samples_template, const Core::TimeWindow &tw);
-
     double coefficient{std::nan("")};
-    int num_samples_template;
-    double sum_template{};
-    double squared_sum_template{};
-    double sum_trace{};
-    double squared_sum_trace{};
-    double sum_template_trace{};
     double lag{}; // seconds
 
     // Time window for w.r.t. the match result
     Core::TimeWindow time_window;
-
-    struct DebugInfo {
-      std::string path_template;
-      std::string path_trace;
-    } debug_info;
-
-    std::string DebugString() const;
   };
 
   void set_filter(Filter *filter) override;
-
-  const Core::TimeSpan init_time() const override;
 
   bool Feed(const Record *record) override;
   void Reset() override;
@@ -77,60 +55,9 @@ protected:
 private:
   StreamState stream_state_;
 
-  // Template waveform starttime
-  Core::Time waveform_start_;
-  // Template waveform endtime
-  Core::Time waveform_end_;
-  // Template waveform
-  Core::TimeSpan waveform_length_;
-  // Template waveform stream id
-  std::string waveform_stream_id_;
-  // Template waveform
-  GenericRecordCPtr waveform_{nullptr};
-  // Template waveform sampling frequency
-  double waveform_sampling_frequency_{0};
-  // Template waveform filter string
-  std::string waveform_filter_;
-  // Template waveform samples squared summed
-  double waveform_squared_sum_{0};
-  // Template waveform samples summed
-  double waveform_sum_{0};
+  // The in-place cross-correlation filter
+  filter::CrossCorrelation<double> cross_correlation_;
 };
-
-class TemplateBuilder : public Builder<Template> {
-public:
-  TemplateBuilder(const std::string &id, const Processor *p);
-  // Set the template waveform of the `Template` waveform processor built.
-  // While `wf_start` and `wf_end` refer to the target template waveform start
-  // and end times, `wf_start_waveform` and `wf_end_waveform` refer to the
-  // actual times of the resulting waveform.
-  TemplateBuilder &
-  set_waveform(WaveformHandlerIfacePtr waveform_handler,
-               const std::string &stream_id, const Core::Time &wf_start,
-               const Core::Time &wf_end,
-               const WaveformHandlerIface::ProcessingConfig &config,
-               Core::Time &wf_start_waveform, Core::Time &wf_end_waveform);
-  TemplateBuilder &set_filter(WaveformProcessor::Filter *filter,
-                              double init_time = 0);
-  TemplateBuilder &set_saturation_check(bool saturation_check,
-                                        double thres = -1);
-
-  // Set the path to the debug info directory
-  TemplateBuilder &set_debug_info_dir(const boost::filesystem::path &path);
-};
-
-namespace template_detail {
-
-/* Calculate the maximum correlation coefficient and corresponding lag from
- * series `tr1` and a series `tr2` where `size_tr1` must be <= `size_tr2`
- * (i.e. `tr1` is cross-correlated with `tr2`).
- */
-bool XCorr(const double *tr1, const int size_tr1, const double *tr2,
-           const int size_tr2, const double sampling_freq,
-           Template::MatchResultPtr &result,
-           const Processor *processor = nullptr);
-
-} // namespace template_detail
 
 } // namespace detect
 } // namespace Seiscomp
