72dc8a76a39df59c4727d6de1760bf393cf4acde
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2021-09-17 08:53:40 +0000

    MAINT: Remove scratch files

--- src/apps/scdetect/test/foo.cpp
+++
@@ -1,8 +0,0 @@
-#define BOOST_TEST_MODULE test_foo
-#include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(test_foo) {
-  BOOST_TEST_MESSAGE("Boost version: " << BOOST_VERSION);
-
-  BOOST_TEST_CHECK(true);
-}
