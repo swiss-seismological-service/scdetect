ed3b69e0dcceff2e5516022c4f87c0dbde53053c
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2021-10-02 07:52:27 +0000

    MAINT: Drop `AnyFixedSlopeRegressionMagnitude`
    Do not use boilerplate template `AnyFixedSlopeRegressionMagnitude`.
    Instead, `std::ratio<>` straight away in
    `FixedSlopeRegressionMagnitude`. Therefore,
    `FixedSlopeRegressionMagnitude` is the template, now.

--- src/apps/scdetect/magnitude/regression.cpp
+++ src/apps/scdetect/magnitude/regression.cpp
@@ -1,6 +1,5 @@
 #include "regression.h"
 
-#include <boost/algorithm/string/join.hpp>
 #include <cfenv>
 #include <cmath>
 #include <vector>
@@ -31,77 +30,15 @@ void RegressionMagnitude::add(
 void RegressionMagnitude::reset() { _amplitudeMagnitudes.clear(); }
 
 /* ------------------------------------------------------------------------- */
-FixedSlopeRegressionMagnitude::FixedSlopeRegressionMagnitude(
-    const std::string& id)
-    : RegressionMagnitude{id} {}
-
-void FixedSlopeRegressionMagnitude::reset() {
-  RegressionMagnitude::reset();
-  _bMean = boost::none;
-}
-
-double FixedSlopeRegressionMagnitude::computeMagnitude(
-    DataModel::Amplitude* amplitude) {
-  if (!_bMean) {
-    recomputeBMean();
-  }
-  return slope() * amplitude->amplitude().value() + *_bMean;
-}
-
-void FixedSlopeRegressionMagnitude::recomputeBMean() {
-  // Given the line formula y = m*x + b and a fixed m for a set of lines (i.e.
-  // both x and y are vectors), the estimate for the interseption b is computed
-  // by:
-  //
-  //  b = mean(y-m*x)
-  //
-  // - currently, the implementation does not take any weighting into account
-  std::feclearexcept(FE_ALL_EXCEPT);
-
-  std::vector<double> b;
-  for (const auto& ampMag : _amplitudeMagnitudes) {
-    const auto& amp{ampMag.amplitude};
-    const auto& mag{ampMag.magnitude};
-
-    const double x{std::log10(amp->amplitude().value())};
-    if (!std::isfinite(x)) {
-      continue;
-    }
-    b.push_back(mag->magnitude().value() - slope() * x);
-  }
-
-  int fe{std::fetestexcept(FE_ALL_EXCEPT)};
-  if ((fe & ~FE_INEXACT) != 0)  // we don't care about FE_INEXACT
-  {
-    std::vector<std::string> exceptions;
-    if (fe & FE_DIVBYZERO) exceptions.push_back("FE_DIVBYZERO");
-    if (fe & FE_INVALID) exceptions.push_back("FE_INVALID");
-    if (fe & FE_OVERFLOW) exceptions.push_back("FE_OVERFLOW");
-    if (fe & FE_UNDERFLOW) exceptions.push_back("FE_UNDERFLOW");
-
-    std::string msg{
-        "Floating point exception during amplitude-magnitude regression: "};
-    msg += boost::algorithm::join(exceptions, ", ");
-    SCDETECT_LOG_WARNING(msg.c_str());
-  }
-
-  if (b.empty()) {
-    throw MagnitudeProcessor::BaseException{"missing data"};
-  }
-
-  _bMean = utils::cma(b.data(), b.size());
-}
-
-/* ------------------------------------------------------------------------- */
 MwFixedSlopeRegressionMagnitude::MwFixedSlopeRegressionMagnitude(
     const std::string& id)
-    : AnyFixedSlopeRegressionMagnitude{id} {
+    : FixedSlopeRegressionMagnitude{id} {
   _type = "Mw";
 }
 
 MLFixedSlopeRegressionMagnitude::MLFixedSlopeRegressionMagnitude(
     const std::string& id)
-    : AnyFixedSlopeRegressionMagnitude{id} {
+    : FixedSlopeRegressionMagnitude{id} {
   _type = "ML";
 }
 
--- src/apps/scdetect/magnitude/regression.h
+++ src/apps/scdetect/magnitude/regression.h
@@ -3,12 +3,17 @@
 
 #include <seiscomp/datamodel/stationmagnitude.h>
 
+#include <boost/algorithm/string/join.hpp>
 #include <boost/optional/optional.hpp>
+#include <cfenv>
+#include <cmath>
 #include <cstdint>
 #include <ratio>
 #include <vector>
 
+#include "../log.h"
 #include "../magnitudeprocessor.h"
+#include "../utils.h"
 
 namespace Seiscomp {
 namespace detect {
@@ -46,46 +51,88 @@ class RegressionMagnitude : public Seiscomp::detect::MagnitudeProcessor {
 
 // Computes a magnitude by means of an *amplitude-magnitude regression* based
 // on a fixed slope
+template <std::intmax_t Num, std::intmax_t Denom = 1>
 class FixedSlopeRegressionMagnitude : public RegressionMagnitude {
  public:
-  FixedSlopeRegressionMagnitude(const std::string& id);
+  FixedSlopeRegressionMagnitude(const std::string& id)
+      : RegressionMagnitude{id} {}
 
  protected:
-  double computeMagnitude(DataModel::Amplitude* amplitude) override;
+  double computeMagnitude(DataModel::Amplitude* amplitude) override {
+    if (!_bMean) {
+      recomputeBMean();
+    }
+    return slope() * amplitude->amplitude().value() + *_bMean;
+  }
 
   // Returns the slope used for calculating the *regression*
-  virtual inline double slope() const = 0;
-
-  void reset() override;
-
-  void recomputeBMean();
-
-  boost::optional<double> _bMean;
-};
-
-template <std::intmax_t Num, std::intmax_t Denom = 1>
-class AnyFixedSlopeRegressionMagnitude : public FixedSlopeRegressionMagnitude {
- public:
-  using FixedSlopeRegressionMagnitude::FixedSlopeRegressionMagnitude;
-
- protected:
-  inline double slope() const override {
-    // TODO(damb): compute at compile time
+  inline double slope() const {
     return static_cast<double>(_slope.num) / static_cast<double>(_slope.den);
   }
+
+  void reset() override {
+    RegressionMagnitude::reset();
+    _bMean = boost::none;
+  }
+
+  void recomputeBMean() {
+    // Given the line formula y = m*x + b and a fixed m for a set of lines (i.e.
+    // both x and y are vectors), the estimate for the interseption b is
+    // computed by:
+    //
+    //  b = mean(y-m*x)
+    //
+    // - currently, the implementation does not take any weighting into account
+    std::feclearexcept(FE_ALL_EXCEPT);
+
+    std::vector<double> b;
+    for (const auto& ampMag : _amplitudeMagnitudes) {
+      const auto& amp{ampMag.amplitude};
+      const auto& mag{ampMag.magnitude};
+
+      const double x{std::log10(amp->amplitude().value())};
+      if (!std::isfinite(x)) {
+        continue;
+      }
+      b.push_back(mag->magnitude().value() - slope() * x);
+    }
+
+    int fe{std::fetestexcept(FE_ALL_EXCEPT)};
+    if ((fe & ~FE_INEXACT) != 0)  // we don't care about FE_INEXACT
+    {
+      std::vector<std::string> exceptions;
+      if (fe & FE_DIVBYZERO) exceptions.push_back("FE_DIVBYZERO");
+      if (fe & FE_INVALID) exceptions.push_back("FE_INVALID");
+      if (fe & FE_OVERFLOW) exceptions.push_back("FE_OVERFLOW");
+      if (fe & FE_UNDERFLOW) exceptions.push_back("FE_UNDERFLOW");
+
+      std::string msg{
+          "Floating point exception during amplitude-magnitude regression: "};
+      msg += boost::algorithm::join(exceptions, ", ");
+      SCDETECT_LOG_WARNING(msg.c_str());
+    }
+
+    if (b.empty()) {
+      throw MagnitudeProcessor::BaseException{"missing data"};
+    }
+
+    _bMean = utils::cma(b.data(), b.size());
+  }
+
+  boost::optional<double> _bMean;
 
  private:
   static const std::ratio<Num, Denom> _slope;
 };
 
 class MwFixedSlopeRegressionMagnitude
-    : public AnyFixedSlopeRegressionMagnitude<2, 3> {
+    : public FixedSlopeRegressionMagnitude<2, 3> {
  public:
   MwFixedSlopeRegressionMagnitude(const std::string& id);
 };
 
 class MLFixedSlopeRegressionMagnitude
-    : public AnyFixedSlopeRegressionMagnitude<1> {
+    : public FixedSlopeRegressionMagnitude<1> {
  public:
   MLFixedSlopeRegressionMagnitude(const std::string& id);
 };
