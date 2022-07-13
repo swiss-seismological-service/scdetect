7e413ed22a66fa3cd30f40f5015facad6dd83a83
Daniel Armbruster <daniel.armbruster@sed.ethz.ch>
2022-07-13 19:26:36 +0000

    MAINT: Implement `processing::StreamConfig`
    Alias `Processing::StreamConfig`.

---
+++ src/apps/cc/processing/stream_config.h
@@ -0,0 +1,16 @@
+#ifndef SCDETECT_APPS_CC_PROCESSING_STREAMCONFIG_H_
+#define SCDETECT_APPS_CC_PROCESSING_STREAMCONFIG_H_
+
+#include <seiscomp/processing/stream.h>
+
+namespace Seiscomp {
+namespace detect {
+namespace processing {
+
+using StreamConfig = Processing::Stream;
+
+}
+}  // namespace detect
+}  // namespace Seiscomp
+
+#endif  // SCDETECT_APPS_CC_PROCESSING_STREAMCONFIG_H_
