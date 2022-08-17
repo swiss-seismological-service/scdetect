swiss-seismological-service/scdetect

commit a1d8ce843ac81a09b82cc4930fc60a7ca639a80f

ENH: Do not validate the entire POT
Implement the arrival offset validation according to the documentation,
i.e. the pick associated with the template waveform processor T_k is
associated if either the POT's row or the POT's column w.r.t. T_k is
valid. This reduces the time complexity to O(n).

src/apps/cc/detector/linker/pot.cpp
@@ -12,6 +12,7 @@
 
 #include "../../util/floating_point_comparison.h"
 #include "../../util/util.h"
+#include "exception.h"
 
 namespace Seiscomp {
 namespace detect {
@@ -19,6 +20,7 @@ namespace detector {
 namespace linker {
 
 const double POT::tableDefault{std::numeric_limits<double>::max()};
+const double POT::tolerance{1e-6};
 
 POT::POT(const std::vector<POT::Entry> &entries) {
   auto sorted{entries};
@@ -89,14 +91,19 @@ void POT::disable(const std::string &processorId) {
   setEnable(processorId, false);
 }
 
-bool POT::validateEnabledOffsets(const POT &other, Core::TimeSpan thres) {
+std::vector<std::string> POT::processorIds() const {
+  return util::map_keys(_processorIdxMap);
+}
+
+bool POT::validateEnabledOffsets(const POT &other,
+                                 const Core::TimeSpan &thres) {
   if (size() != other.size()) {
-    return false;
+    throw BaseException{"invalid POT size"};
   }
 
   if (util::map_keys(_processorIdxMap) !=
       util::map_keys(other._processorIdxMap)) {
-    return false;
+    throw BaseException{"processor id mismatch"};
   }
 
   // create mask with common enabled processors
@@ -108,7 +115,6 @@ bool POT::validateEnabledOffsets(const POT &other, Core::TimeSpan thres) {
   }
   auto mask{createMask(enabledProcessors)};
 
-  constexpr double tolerance{1.0e-6};
   for (size_type i{0}; i < size(); ++i) {
     for (size_type j{0}; j < size(); ++j) {
       if (mask[i][j] && validEntry(_offsets[i][j]) &&
@@ -120,6 +126,45 @@ bool POT::validateEnabledOffsets(const POT &other, Core::TimeSpan thres) {
     }
   }
 
+  return true;
+}
+
+bool POT::validateEnabledOffsets(const std::string &processorId,
+                                 const std::vector<double> &otherOffsets,
+                                 const std::vector<bool> &otherMask,
+                                 const Core::TimeSpan &thres) {
+  if (otherOffsets.size() != size() || otherMask.size() != size()) {
+    throw BaseException{"invalid sizes"};
+  }
+
+  auto it{_processorIdxMap.find(processorId)};
+  if (it == _processorIdxMap.end()) {
+    throw BaseException{"unknown processor id: " + processorId};
+  }
+
+  if (!enabled(processorId)) {
+    return false;
+  }
+
+  // create mask with common enabled processors
+  std::vector<bool> common_mask(size(), false);
+  size_type i{0};
+  for (const auto &p : _processorIdxMap) {
+    if (p.second.enabled && otherMask[i]) {
+      common_mask[i++] = true;
+    }
+  }
+
+  const auto &offsets{_offsets[std::distance(_processorIdxMap.begin(), it)]};
+
+  for (size_type i{0}; i < size(); ++i) {
+    if (common_mask[i] && validEntry(otherOffsets[i]) &&
+        validEntry(offsets[i]) &&
+        util::greaterThan(std::abs(offsets[i] - otherOffsets[i]),
+                          static_cast<double>(thres), tolerance)) {
+      return false;
+    }
+  }
   return true;
 }
 

src/apps/cc/detector/linker.cpp
@@ -7,6 +7,8 @@
 #include <unordered_set>
 
 #include "../util/math.h"
+#include "../util/util.h"
+#include "detail.h"
 
 namespace Seiscomp {
 namespace detect {
@@ -180,8 +182,11 @@ void Linker::process(const TemplateWaveformProcessor *proc,
         if (newPick ||
             resultIt->coefficient > it->second.resultIt->coefficient) {
           if (_thresArrivalOffset) {
-            auto cmp{createCandidatePOT(*candidateIt, procId, result)};
-            if (!_pot.validateEnabledOffsets(cmp, *_thresArrivalOffset)) {
+            auto candidatePOTData{
+                createCandidatePOTData(*candidateIt, procId, result)};
+            if (!_pot.validateEnabledOffsets(procId, candidatePOTData.offsets,
+                                             candidatePOTData.mask,
+                                             *_thresArrivalOffset)) {
               continue;
             }
           }
@@ -276,6 +281,41 @@ linker::POT Linker::createCandidatePOT(
   }
 
   return linker::POT(candidatePOTEntries);
+}
+
+Linker::CandidatePOTData Linker::createCandidatePOTData(
+    const Candidate &candidate, const std::string &processorId,
+    const linker::Association::TemplateResult &newResult) {
+  auto allProcessorIds{_pot.processorIds()};
+  const auto &associatedCandidateTemplateResults{candidate.association.results};
+
+  auto associatedProcessorIds{
+      util::map_keys(associatedCandidateTemplateResults)};
+  std::set<detail::ProcessorIdType> enabledProcessorIds{
+      associatedProcessorIds.begin(), associatedProcessorIds.end()};
+  enabledProcessorIds.emplace(processorId);
+
+  CandidatePOTData ret(allProcessorIds.size());
+  for (std::size_t i{0}; i < allProcessorIds.size(); ++i) {
+    const auto &curProcessorId{allProcessorIds[i]};
+    bool disabled{enabledProcessorIds.find(curProcessorId) ==
+                  enabledProcessorIds.end()};
+    if (disabled) {
+      continue;
+    }
+
+    if (curProcessorId != processorId) {
+      ret.offsets[i] = std::abs(static_cast<double>(
+          associatedCandidateTemplateResults.at(curProcessorId)
+              .arrival.pick.time -
+          newResult.arrival.pick.time));
+    } else {
+      ret.offsets[i] = 0;
+    }
+    ret.mask[i] = true;
+  }
+
+  return ret;
 }
 
 /* ------------------------------------------------------------------------- */

src/apps/cc/detector/linker.h
@@ -91,6 +91,18 @@ class Linker {
       const Candidate &candidate, const std::string &processorId,
       const linker::Association::TemplateResult &newResult);
 
+  struct CandidatePOTData {
+    std::vector<double> offsets;
+    std::vector<bool> mask;
+
+    CandidatePOTData() = default;
+    explicit CandidatePOTData(std::size_t n)
+        : offsets(n, linker::POT::tableDefault), mask(n, false) {}
+  };
+  CandidatePOTData createCandidatePOTData(
+      const Candidate &candidate, const std::string &processorId,
+      const linker::Association::TemplateResult &newResult);
+
   // `TemplateWaveformProcessor` processor
   struct Processor {
     const TemplateWaveformProcessor *proc;

src/apps/cc/detector/linker/exception.cpp
@@ -1,0 +1,13 @@
+#include "exception.h"
+
+namespace Seiscomp {
+namespace detect {
+namespace detector {
+namespace linker {
+
+BaseException::BaseException() : Exception{"base linker exception"} {}
+
+}  // namespace linker
+}  // namespace detector
+}  // namespace detect
+}  // namespace Seiscomp

src/apps/cc/detector/linker/exception.h
@@ -1,0 +1,22 @@
+#ifndef SCDETECT_APPS_CC_DETECTOR_LINKER_EXCEPTION_H_
+#define SCDETECT_APPS_CC_DETECTOR_LINKER_EXCEPTION_H_
+
+#include "../../exception.h"
+
+namespace Seiscomp {
+namespace detect {
+namespace detector {
+namespace linker {
+
+class BaseException : public Exception {
+ public:
+  using Exception::Exception;
+  BaseException();
+};
+
+}  // namespace linker
+}  // namespace detector
+}  // namespace detect
+}  // namespace Seiscomp
+
+#endif  // SCDETECT_APPS_CC_DETECTOR_LINKER_EXCEPTION_H_

src/apps/cc/detector/linker/pot.h
@@ -59,16 +59,36 @@ class POT {
   // Disables the POT for the processor identified by `processorId`
   void disable(const std::string& processorId);
 
+  // Returns the processor identifiers based on the internal sort order
+  std::vector<std::string> processorIds() const;
+
   // Validate pick offsets of this POT with `other` where pick offsets must be
-  // smaller than `thres`.
+  // smaller than or equal to `thres`.
   //
   // - only validates those offsets which are *enabled* both in `this` and
-  // `other`
+  // `other`.
   // - returns `true` if the validation was successful or `false` if not,
   // respectively.
-  bool validateEnabledOffsets(const POT& other, Core::TimeSpan thres);
+  bool validateEnabledOffsets(const POT& other, const Core::TimeSpan& thres);
+
+  // Validates pick offsets of this POT with the `otherOffsets` regarding the
+  // processor identified by `processorId` where pick offsets must be smaller
+  // than or equal to `thres`.
+  //
+  // - both the offsets defined by `otherOffsets` and the masks from
+  // `otherMask` must be sorted by the corresponding template waveform
+  // processor identifier.
+  // - only validates those `otherOffsets` which are *enabled* both in `this`
+  // and `otherMask`.
+  // - returns `true` if the validation was successful or `false` if not,
+  // respectively.
+  bool validateEnabledOffsets(const std::string& processorId,
+                              const std::vector<double>& otherOffsets,
+                              const std::vector<bool>& otherMask,
+                              const Core::TimeSpan& thres);
 
  private:
+  static const double tolerance;
 
   bool validEntry(double e) const;
 

commit 51695d62b6dbf65abfb24631f6dd74523dcd9f08

BUG: Correctly create mask
Note that this bug was introduced with
a1d8ce843ac81a09b82cc4930fc60a7ca639a80f. That is, while improving the
arrival offset validation performance wise.

src/apps/cc/detector/linker/pot.cpp
@@ -146,19 +146,18 @@ bool POT::validateEnabledOffsets(const std::string &processorId,
     return false;
   }
 
+  std::vector<bool> commonMask(size());
   // create mask with common enabled processors
-  std::vector<bool> common_mask(size(), false);
   size_type i{0};
-  for (const auto &p : _processorIdxMap) {
-    if (p.second.enabled && otherMask[i]) {
-      common_mask[i++] = true;
-    }
+  for (const auto &idxPair : _processorIdxMap) {
+    commonMask[i] = idxPair.second.enabled && otherMask[i];
+    ++i;
   }
 
   const auto &offsets{_offsets[std::distance(_processorIdxMap.begin(), it)]};
 
   for (size_type i{0}; i < size(); ++i) {
-    if (common_mask[i] && validEntry(otherOffsets[i]) &&
+    if (commonMask[i] && validEntry(otherOffsets[i]) &&
         validEntry(offsets[i]) &&
         util::greaterThan(std::abs(offsets[i] - otherOffsets[i]),
                           static_cast<double>(thres), tolerance)) {